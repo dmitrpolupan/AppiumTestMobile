d182993f16b7997ffdb2717e933e929fbcbf8581
dmitrpolupan <dmitr.polupan@gmail.com>
2022-04-16 21:38:37 +0300

    Ex18

--- src/test/java/lib/ui/mobile_web/MWSearchPageObject.java
+++ src/test/java/lib/ui/mobile_web/MWSearchPageObject.java
@@ -11,12 +11,11 @@ public class MWSearchPageObject extends SearchPageObject
         SEARCH_RESULT_LOCATOR = "css:ul.page-list>li.page-summary";
         SEARCH_INPUT = "css:form>input[type=search]"; //+
         SEARCH_CANCEL_BUTTON = "css:div>button.cancel"; //+
-        //SEARCH_RESULT = "xpath://div[contains(@class, 'wikidata-description')][contains(text(), '{SUBSTRING}')]"; //+
         SEARCH_RESULT = "xpath://li[contains(@class, 'page-summary')]//*[contains(@data-title, '{SUBSTRING}') or contains(text(), '{SUBSTRING}')]";
         NO_RESULTS_LABEL = "css:p.without-results"; //+
         SEARCH_RESULT_ELEMENT = "css:ul.page-list>li.page-summary"; //+
         SEARCH_RESULT_ARTICLES_NAMES = "css:ul.page-list>li.page-summary"; //+
-        SEARCH_RESULT_LOCATOR_FOR_TITLE_DESCRIPTION_TPL = "xpath://*[contains(@resource-id, 'page_list_item_title')][contains(@text, '{TITLE}')]//parent::*//*[contains(@resource-id, 'page_list_item_description')][contains(@text, '{DESCRIPTION}')]";
+        SEARCH_RESULT_LOCATOR_FOR_TITLE_DESCRIPTION_TPL = "xpath://li[contains(@class, 'page-summary')]//a[@data-title='{TITLE}']//div[text() = '{DESCRIPTION}']"; //*
     }
 
     public MWSearchPageObject(RemoteWebDriver driver){
